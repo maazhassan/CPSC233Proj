b349bdfeb6ad63a21a4849d8502c1920025dc8c6
Vianney Nguyen <vianney.nguyen@ucalgary.ca>
2020-02-21 12:16:41 -0700

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- CPSC233Project/src/Pieces/Rook.java
+++ CPSC233Project/src/Pieces/Rook.java
@@ -25,47 +25,49 @@ public class Rook extends Piece {
 		int endX = move.getEnd().getX();
 		int startY = move.getStart().getY();
 		int endY = move.getEnd().getY();
+
 		
-		// check if it can move
-		if (Math.abs(startX - endX) < 8 - startX && Math.abs(startY - endY) < 8 - startY) {
-			return true;
-		}
 		
-		if (startX == endX) {
+		if (startY == endY) {
 			// horizontally
-			if (startY < endY) {
+			if (startX < endX) {
 				// move right
-				for (int i = startY + 1; i <= endY; i++) {
-					if (board.getSquare(startY, i).getPiece() != null) {
+				for (int i = startX + 1; i <= endX; i++) {
+					if (board.getSquare(startX, i).getPiece() != null) {
 						return false;
 					}
 				}
 			} else {
 				// move left
-				for (int i = startY - 1; i >= endY; i++) {
-					if (board.getSquare(startY, i).getPiece() != null) {
+				for (int i = startX - 1; i >= endX; i--) {
+					if (board.getSquare(startX, i).getPiece() != null) {
 						return false;
 					}
 				}
 			}
 			
-		} else if (startY == endY) {
+		} else if (startX == endX) {
 			//vertical
-			if (startX < endX) {
-				// move backward
-				for (int i = startX + 1; i <= endX; i++) {
-					if (board.getSquare(i, startX).getPiece() != null) {
+			if (startY < endY) {
+				// move down
+				for (int i = startY + 1; i <= endY; i++) {
+					if (board.getSquare(i, startY).getPiece() != null) {
 						return false;
 					}
 				}
 			} else {
 				// move forward
-				for (int i = startX - 1; i >= endX; i++) {
-					if (board.getSquare(i, startX).getPiece() != null) {
+				for (int i = startY - 1; i >= endY; i--) {
+					if (board.getSquare(i, startY).getPiece() != null) {
 						return false;
 					}
 				}
 			}
+		}
+
+        // check if it can move
+		if (Math.abs(startX - endX) < 8 - startX && Math.abs(startY - endY) < 8 - startY) {
+			return true;
 		}
 				
 		return false;
