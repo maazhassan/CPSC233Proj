585a32b6d24bfbb75f3a4541f66e33c1ac4d5f72
Vianney Nguyen <nguyenvianney@gmail.com>
2020-03-05 14:46:09 -0700

    Create a Controller for the JavaFX app

--- CPSC233Project/src/JavaFXApp.java
+++ CPSC233Project/src/JavaFXApp.java
@@ -1,11 +1,39 @@
+import Game.GameEventHandler;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class JavaFXApp extends Application {
+    private Controller controller;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+        controller = new Controller(this);
 
         primaryStage.setTitle("Hello World");
         primaryStage.show();
+    }
+}
+
+class Controller implements GameEventHandler {
+
+    private JavaFXApp window;
+
+    public Controller(JavaFXApp window) {
+        this.window = window;
+    }
+
+    @Override
+    public boolean requestShouldPlayAgain() {
+        return false;
+    }
+
+    @Override
+    public int[] createMove() {
+        return new int[0];
+    }
+
+    @Override
+    public void log(String out) {
+
     }
 }
