06a8a639e47e68af1408120890407d15869d0c23
Vianney Nguyen <nguyenvianney@gmail.com>
2020-03-05 14:44:05 -0700

    Display basic JavaFX application

---
+++ CPSC233Project/src/JavaFXApp.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JavaFXApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        primaryStage.setTitle("Hello World");
+        primaryStage.show();
+    }
+}
